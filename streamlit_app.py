c61a442a9ae546f26e8bda7894ddb89c65c234af
Adriele Pinto <97919969+adrielepinto@users.noreply.github.com>
2023-09-02 14:11:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import streamlit         as st
 
 
 # Import Data Frames
-df_bp = pd.read_csv( '/business_performance.csv' )
+df_bp = pd.read_csv( '/models/business_performance.csv' )
 df_bp = df_bp.drop( 'Unnamed: 0', axis=1 )
 df_c  = pd.read_csv( '/models/crossprice.csv' )
 df_e  = pd.read_csv( '/models/df_elasticity.csv' )
